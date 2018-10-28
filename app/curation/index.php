bfbf75a355ae408537494353cd7afac386834087
John <john.sturdivant@gmail.com>
2018-10-28 18:06:22 -0500

    sunday pm

--- app/curation/index.php
+++ app/curation/index.php
@@ -6,8 +6,22 @@
     <select name='selectedAction' id='selectedAction' onchange='showContainer()'>
         <option></option>
         <option value='addAsset' >Add Asset</option>
+        <option value='editAsset' >Edit Asset</option>
         <option value='addTask'>Add Task</option>
     </select>
+    <div class='col-6-lg' id='editAssetContainer' style='display: none'>
+        <h3>Select Assets:</h3>
+
+        <select name='selectAssetId' id='selectAssetId'>
+            <?php
+                $assets = queryStmtToArray('SELECT id, name FROM enhome.assets');
+                foreach ($assets as $b){
+                    echo "<option value='".$b['id']."'>".$b['name']."</option>";
+                }
+            ?>
+        </select>
+        <input type='submit'>
+    </div>
     <div class='col-6-lg' id='addAssetContainer' style='display: none'>
         <h1>Add Asset</h1>
         <h3>Branch:</h3>
@@ -19,8 +33,21 @@
                 }
             ?>
         </select>
+        <h3>Asset Make:</h3>
+        <select name='selectAssetMakeId' id='selectAssetMakeId'>
+            <?php
+                $makes = queryStmtToArray('SELECT id, name FROM enhome.asset_makes');
+                foreach ($makes as $b){
+                    echo "<option value='".$b['id']."'>".$b['name']."</option>";
+                }
+            ?>
+        </select>
         <h3>Asset Name:</h3>
         <input type='text' name='assetName' id='assetName'>
+        <h3>Model No.:</h3>
+        <input type='text' name='assetModelNo' id='assetModelNo'>
+        <h3>Model Year(s):</h3>
+        <input type='text' name='assetModelYear' id='assetModelYear'>
         <h3>Asset Description:</h3>
         <textarea name='assetDescription' id='assetDescription'></textarea>
         <h3>Asset Image Address:</h3>
@@ -69,13 +96,50 @@
         document.getElementById("addTaskContainer").style.display="none";
         if(selectedAction == 'addAsset'){
             document.getElementById("addAssetContainer").style.display="block";
+        } else if(selectedAction == 'editAsset'){
+            document.getElementById("editAssetContainer").style.display="block";
+            document.getElementById("addAssetContainer").style.display="block";
+            var editAssetId = get('assetId');
+            populateAssetEdit(editAssetId);
         } else if(selectedAction == 'addTask'){
             document.getElementById("addTaskContainer").style.display="block";
         }
     }
+
+    function populateAssetEdit(assetId){
+        var assetDetail = <?php print_r( getAssetDetail($_POST['selectAssetId'])); ?>;
+
+        document.getElementById('assetName').value = assetDetail.assetName;
+        document.getElementById('selectedBranchId').value = assetDetail.branchId;
+        document.getElementById('selectAssetMakeId').value = assetDetail.makeId;
+        document.getElementById('assetModelNo').value = assetDetail.modelNo;
+        document.getElementById('assetModelYear').value = assetDetail.modelYear;
+        document.getElementById('assetDescription').value = assetDetail.detail.description;
+        document.getElementById('assetImageAddress').value = assetDetail.detail.otherDetails[0].value;
+
+        return assetIdDetail;
+    }
+</script>
+<script>
+    var selectedAction = '<?php echo $_POST['selectedAction']; ?>';
+    var selectedActionContainer = document.getElementById('selectedAction');
+    selectedActionContainer.value = selectedAction;
+    showContainer();
+
 </script>
 
 <?php
+    function getAssetDetail($assetId){
+        $dbh = db_connect();
+        $assetDetailQuery = "SELECT
+            assets.id, branch_id as branchId, assets.name as assetName, make_id as makeId, asset_makes.name as asset_make, model_no as modelNo, model_year as modelYear, detail, added_at, updated_at, deleted_at
+            FROM enhome.assets
+            LEFT JOIN asset_makes ON assets.make_id = asset_makes.id WHERE assets.id = $assetId;";
+        $assetDetail = queryStmtToArray($assetDetailQuery)[0];
+        $assetDetail['detail'] = json_decode($assetDetail['detail']);
+        return json_encode($assetDetail);
+    }
+
     if($_POST['selectedAction'] == 'addAsset'){
         addAssetToLibrary($_POST);
     } elseif($_POST['selectedAction'] == 'addTask'){
@@ -133,6 +197,9 @@
             (
                 `branch_id`,
                 `name`,
+                `make_id`,
+                `model_no`,
+                `model_year`,
                 `detail`,
                 `added_at`,
                 `updated_at`
@@ -141,6 +208,9 @@
             (
                 '".$data['selectedBranchId']."',
                 '".$data['assetName']."',
+                '".$data['selectAssetMakeId']."',
+                '".$data['assetModelNo']."',
+                '".$data['assetModelYear']."',
                 '".$data['detail']."',
                 now(),
                 now()
